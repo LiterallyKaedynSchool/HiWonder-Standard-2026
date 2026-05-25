6d6a468578604fb7d0c3dc6446cc94fe864cee17
Kaedyn Eastall (School) <eask23083@wainuiomatahigh.school.nz>
2026-05-26 10:29:42 +1200

    Use SAFE_DIST constant for movement decision
    Replaced hardcoded distance with SAFE_DIST constant for better maintainability.

--- version-5/lesson.py
+++ version-5/lesson.py
@@ -9,6 +9,9 @@ from lesson_header import *
 # Cup counter (starting at 0)
 cups = 0
 
+# Safe movement distance
+SAFE_DIST = 15
+
 # Define dict_logs as an empty dictionary
 dict_logs = {}
 
@@ -30,7 +33,7 @@ distance_cm = sonar.get_distance_cm(filtered=True)
 while cups < 4:
     distance_cm = sonar.get_distance_cm(filtered=True)
     dict_logs["[DEBUG] Started"] = True
-    if distance_cm < 15:
+    if distance_cm < SAFE_DIST:
         if d_m[cups] == "right":
             # Move the bot
             moves.move_right(1.5)
